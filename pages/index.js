384cdd2edd0e1341514dc841e119d13d7964a888
thasup <thanachonfirst@hotmail.com>
2022-09-24 22:49:52 +0700

    Add homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>
+import factory from '../ethereum/factory';
+
+function HomePage({ campaigns }) {
+  return (<>
+    <div>Welcome to Next.js!</div>
+    <div>First campaign : {campaigns[0]}</div>
+  </>)
+}
+
+export async function getServerSideProps() {
+  const campaigns = await factory.methods.getDeployedCampaigns().call();
+
+  return { props: { campaigns } };
 }
 
 export default HomePage
