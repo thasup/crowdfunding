f3959918f3c1a835a1e7bfc77c025bfba5b826ff
thasup <thanachonfirst@hotmail.com>
2022-10-01 20:26:38 +0700

    Create navigation bar

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Icon, Input, Menu } from 'semantic-ui-react'
 
 const Header = () => {
+  const [activeItem, setActiveItem] = useState("");
+
+  const handleItemClick = (e, { name }) => {
+    setActiveItem(name);
+  }
   return (
-    <div>Header</div>
+    <Menu className='navbar' size='large' fluid>
+      <Menu.Item header>CreativeFund</Menu.Item>
+      <Menu.Item
+        name='discover'
+        active={activeItem === 'discover'}
+        onClick={handleItemClick}
+      />
+      <Menu.Item
+        name='aboutUs'
+        active={activeItem === 'aboutUs'}
+        onClick={handleItemClick}
+      />
+      <Menu.Item
+        name='help'
+        active={activeItem === 'help'}
+        onClick={handleItemClick}
+      />
+
+      <Menu.Menu position='right'>
+        <Input className='icon input-search' icon='search' placeholder='Search campaign...' />
+        <Menu.Item
+          name='contact'
+          active={activeItem === 'contact'}
+          onClick={handleItemClick}
+        >
+          <Icon name='add' />
+        </Menu.Item>
+      </Menu.Menu>
+    </Menu>
   )
 }
 
