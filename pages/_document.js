ee03996a4da4d2009b82acdd751874c3d7ccaef4
thasup <thanachonfirst@hotmail.com>
2022-10-09 00:51:27 +0700

    Fix favicon do not show properly

--- pages/_document.js
+++ pages/_document.js
@@ -7,10 +7,10 @@ export default function Document() {
 				<meta charSet="utf-8" />
 				<meta name="description" content="creative is nothingbut a mind set free." />
 				{/* <link rel="icon" href="/asset/favicon.ico" type="image/x-icon" /> */}
-				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-				<link rel="manifest" href="/site.webmanifest" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/asset/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/asset/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/asset/favicon-16x16.png" />
+				<link rel="manifest" href="/asset/site.webmanifest" />
 				<link
 					async
 					rel="stylesheet"
