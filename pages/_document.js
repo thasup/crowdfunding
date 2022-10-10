951fa982a7110b753f6ab5d88cbc057953c27072
thasup <thanachonfirst@hotmail.com>
2022-10-11 00:02:56 +0700

    Add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,24 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
+				<script
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=G-RWZR81B8RW"
+				/>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RWZR81B8RW', {
+              page_path: window.location.pathname,
+            });
+          `,
+					}}
+				/>
 				<meta charSet="utf-8" />
-				<meta name="description" content="creative is nothingbut a mind set free." />
+				<meta name="description" content="creative is nothing but a mind set free." />
 				{/* <link rel="icon" href="/asset/favicon.ico" type="image/x-icon" /> */}
 				<link rel="apple-touch-icon" sizes="180x180" href="/asset/apple-touch-icon.png" />
 				<link rel="icon" type="image/png" sizes="32x32" href="/asset/favicon-32x32.png" />
