f8f5f63d28fdb235eecf91e7fb90403bba9bbb59
thasup <thanachonfirst@hotmail.com>
2022-10-10 15:55:42 +0700

    Add requests list table

---
+++ components/RequestRow.js
@@ -0,0 +1,53 @@
+import { Table, Button } from "semantic-ui-react";
+import web3 from "../ethereum/web3";
+import Campaign from "../ethereum/campaign";
+
+const { Row, Cell } = Table;
+
+export const RequestRow = ({ request, id, address, approversCount }) => {
+  const { description, value, recipient, complete, approvalCount } = request;
+
+  const campaign = Campaign(address);
+
+  const handleApproveRequest = async () => {
+    const accounts = await web3.eth.getAccounts();
+    await campaign.methods.approveRequest(id).send({
+      from: accounts[0],
+    });
+  };
+
+  const handleFinalize = async () => {
+    const accounts = await web3.eth.getAccounts();
+    await campaign.methods.finalizeRequest(id).send({
+      from: accounts[0],
+    });
+  };
+
+  const readyToFinalize = !complete && approvalCount >= approversCount / 2;
+
+  return (
+    <Row disabled={complete} positive={readyToFinalize}>
+      <Cell>{id}</Cell>
+      <Cell>{description}</Cell>
+      <Cell>{web3.utils.fromWei(value, "ether")}</Cell>
+      <Cell>{recipient}</Cell>
+      <Cell>{`${approvalCount}/${approversCount}`}</Cell>
+      <Cell>
+        {!complete ? (
+          <Button onClick={handleApproveRequest} color="green" basic>
+            Approve
+          </Button>
+        ) : (
+          <p>Approved</p>
+        )}
+      </Cell>
+      <Cell>
+        {readyToFinalize && (
+          <Button onClick={handleFinalize} color="teal" basic>
+            Finalize
+          </Button>
+        )}
+      </Cell>
+    </Row>
+  );
+};
--- pages/campaigns/[address]/requests/index.js
+++ pages/campaigns/[address]/requests/index.js
@@ -1,14 +1,49 @@
 import Link from 'next/link'
 import React from 'react'
-import { Button } from 'semantic-ui-react'
+import { Button, Table } from 'semantic-ui-react'
 
-const requestsDetail = ({ contractAddress }) => {
+import Campaign from "../../../../ethereum/campaign"
+import { RequestRow } from "../../../../components/RequestRow";
+
+const { Header, Row, HeaderCell, Body } = Table;
+
+const requestsDetail = ({ contractAddress, requestsCount, requests, approversCount }) => {
+  const renderRows = () =>
+    requests.map((request, index) => {
+      return (
+        <RequestRow
+          request={request}
+          key={index}
+          id={index}
+          address={contractAddress}
+          approversCount={approversCount}
+        />
+      );
+    });
+
   return (
     <div>
       <h2>Request List</h2>
       <Link href={"/campaigns/[address]/requests/new"} as={`/campaigns/${contractAddress}/requests/new`}>
-        <Button primary>Add Request</Button>
+        <Button primary floated="right" style={{ marginBottom: "10px" }}>Add Request</Button>
       </Link>
+
+      <Table>
+        <Header>
+          <Row>
+            <HeaderCell>ID</HeaderCell>
+            <HeaderCell>Description</HeaderCell>
+            <HeaderCell>Amount</HeaderCell>
+            <HeaderCell>Recipient</HeaderCell>
+            <HeaderCell>Approval Count</HeaderCell>
+            <HeaderCell>Approve</HeaderCell>
+            <HeaderCell>Finalize</HeaderCell>
+          </Row>
+        </Header>
+        <Body>{renderRows()}</Body>
+      </Table>
+
+      <div>Found {requestsCount} requests</div>
     </div>
   )
 };
@@ -16,9 +51,29 @@ const requestsDetail = ({ contractAddress }) => {
 export async function getServerSideProps(context) {
   const { address } = context.query;
 
+  const campaign = Campaign(address);
+  const requestsCount = await campaign.methods.getRequestsCount().call();
+  const approversCount = await campaign.methods.approversCount().call();
+
+  const jsonRequests = await Promise.all(
+    Array(parseInt(requestsCount)).fill().map((element, index) => {
+      return campaign.methods.requests(index).call()
+    })
+  );
+  const requests = JSON.parse(JSON.stringify(jsonRequests));
+
+  const sanitizedRequests = requests.map(
+    ({ description, value, recipient, complete, approvalCount }) => {
+      return { description, value, recipient, complete, approvalCount };
+    }
+  );
+
   return {
     props: {
       contractAddress: address,
+      requestsCount: sanitizedRequests.length || 0,
+      approversCount,
+      requests: sanitizedRequests || [],
     }
   };
 }
